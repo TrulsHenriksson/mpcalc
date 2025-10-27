7274e36657ed55dffb25d6333838d23b67155bc7
Truls Henriksson <trulshenriksson@gmail.com>
2025-10-27 21:52:13 +0100

    Set debug print spacing to 25

--- mpcalc.py
+++ mpcalc.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     ALL_DIGITS = args.all
     DEBUG = args.debug
 
-    if DEBUG: print(f"{'Input:':<22}", expression)
+    if DEBUG: print(f"{'Input:':<25}", expression)
 
     if match := disallowed_characters.search(expression):
         raise ValueError(f"Character not allowed: {match[0]}")
@@ -69,16 +69,16 @@ if __name__ == "__main__":
     with mp.workdps(DIGITS):
         # Add mp. before names that are not escaped by backslashes
         expression = re.sub(name_pattern, r"mp.\1", expression)
-        if DEBUG: print(f"{'Replaced names:':<22}", expression)
+        if DEBUG: print(f"{'Replaced names:':<25}", expression)
 
         # Convert numbers to mpf or mpc
         expression = re.sub(number_pattern, replace_number_with_mpf, expression)
-        if DEBUG: print(f"{'Replaced numbers:':<22}", expression)
-        if DEBUG: print(f"{'Contents of NUMBERS:':<22}", NUMBERS)
+        if DEBUG: print(f"{'Replaced numbers:':<25}", expression)
+        if DEBUG: print(f"{'Contents of NUMBERS:':<25}", NUMBERS)
 
         # Strip backslashes
         expression = expression.replace("\\", "")
-        if DEBUG: print(f"{'Removed backslashes:':<22}", expression)
+        if DEBUG: print(f"{'Removed backslashes:':<25}", expression)
 
         value = eval(expression, globals={"NUMBERS": NUMBERS, "mp": mp})
         if DEBUG:
